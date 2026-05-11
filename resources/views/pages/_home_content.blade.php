72b828a17f3049ee73d36325bab88628db11d8c2
Manlikenonso1k <victorynonso9@gmail.com>
2026-05-11 10:26:12 +0100

    Remove duplicate mobile nav initialization

--- resources/views/pages/_home_content.blade.php
+++ resources/views/pages/_home_content.blade.php
@@ -1650,7 +1650,3 @@
       </div>
       <hr>
     </section>
-
-    <script src="js/jquery-1.11.0.min.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="js/script.js"></script>
